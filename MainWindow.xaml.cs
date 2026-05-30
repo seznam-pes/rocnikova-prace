b30ff4feed9958f08593168a3d9daefda3452401
seznam pes <frantisekburda@icloud.com>
2026-05-30 10:39:38 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.VisualBasic.Devices;
-using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 using System.Configuration.Internal;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
@@ -19,6 +17,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static WpfApp1.Scenes;
 
+using Keyboard = System.Windows.Input.Keyboard;
+
 namespace WpfApp1
 {
     /// <summary>
@@ -33,31 +33,34 @@ namespace WpfApp1
             title_menu.Visibility = Visibility.Visible;
             game.Visibility = Visibility.Collapsed;
             animace = new Animations(this);
-            dialogy = new Dialogues();
-            sceny = new Scenes(this, dialogy, animace);
+            sceny = new Scenes(this, animace);
+
+            this.KeyDown += (s, e) =>
+            {
+                if (e.Key == Key.Enter)
+                {
+                    EnterPressed();
+                }
+            };
         }
 
-        public class localAxis()
-        {
-            public int localX;
-            public int localY;
-        }
-
-        Dialogues dialogy;
         Animations animace;
         Scenes sceny;
+
         public List<Button> buttonlist = new List<Button>();
-        private List<Image> inventorySlots;
-        private List<int> inventoryItems = new();
+        public int currentSceneId = 0;
         public int progress = 0;
         string str_progress = "0";
-        public List<string> availableitems = new List<string>
-        {
-            {"pack://application:,,,/img/items/key.png" },
-            {"placeholder"},
-        };
 
+        public bool hasKey = false;
+        public bool trezorLock = true;
+        public bool letterRead = false;
+        public bool alreadySwitched1 = false;
+        public bool barmanTalk = false;
+        public bool clownTalk = false;
 
+        private const string spravnykod = "26643";
+        private string enteredCode = "";
 
 
 
@@ -82,52 +85,27 @@ namespace WpfApp1
 
 
 
-        public void InitInventory()
-        {
-            inventorySlots = new List<Image> { slot1, slot2, slot3 };
-        }
 
-        public void AddItem(int itemId)
-        {
-            if (inventoryItems.Contains(itemId)) return;
-
-            inventoryItems.Add(itemId);
-            int slotIndex = inventoryItems.Count - 1;
-            inventorySlots[slotIndex].Source = new BitmapImage(new Uri(availableitems[itemId]));
-        }
-
-        public bool HasItem(int itemId)
-        {
-            return inventoryItems.Contains(itemId);
-        }
-
-
-
-
-
-
-
-        public void LaunchDialogue(int id, int sleep)
+        public async Task LaunchDialogue(string promptedtext, int sleep)
         {
             dialoguebg.Visibility = Visibility.Visible;
             dialoguebar.Visibility = Visibility.Visible;
-            dialoguebar.Text = dialogy.dialogues[id];
-            Thread.Sleep(sleep);
-            dialoguebg.Visibility = Visibility.Collapsed;
-            dialoguebar.Visibility = Visibility.Collapsed;
+            dialoguehelp.Visibility = Visibility.Visible;
+            dialoguebar.Text = promptedtext;
+            await Task.Delay(sleep*1000);
+            dialoguebg.Visibility = Visibility.Hidden;
+            dialoguebar.Visibility = Visibility.Hidden;
+            dialoguehelp.Visibility = Visibility.Hidden;
+            dialoguebar.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE8CA00"));
         }
 
-        private Stack<int> history = new();
-        private int currentSceneId = 0;
-
-        public void GoToScene(int sceneId)
+        public void LoadScene(int sceneId)
         {
-            LoadScene(sceneId);
-        }
+            if (sceneId == 3 && trezorLock == false)
+            {
+                LoadScene(4);
+            }
 
-
-        private void LoadScene(int sceneId)
-        {
             if (sceneId == 3)
             {
                 trezor.Visibility = Visibility.Visible; game.Visibility = Visibility.Collapsed;
@@ -140,7 +118,7 @@ namespace WpfApp1
             currentSceneId = sceneId;
             var scene = sceny.scenes.First(s => s.Id == sceneId);
 
-            pozadi.Source = new BitmapImage(new Uri(scene.BackgroundPath, UriKind.RelativeOrAbsolute));
+            pozadi.Source = new BitmapImage(new Uri(scene.BackgroundPath));
 
             foreach (var btn in buttonlist)
                 main.Children.Remove(btn);
@@ -148,6 +126,9 @@ namespace WpfApp1
 
             foreach (var data in scene.Buttons)
                 CreateButton(data);
+
+            progress = sceneId;
+            WriteSave();
         }
 
         private void CreateButton(ButtonData data)
@@ -165,10 +146,128 @@ namespace WpfApp1
                 Margin = new Thickness(data.X, data.Y, 0, 0)
             };
 
-            if (data.IsDialogue) button.Click += (s, e) => LaunchDialogue(data.DialogueId, data.Sleep);
-            button.Click += (s, e) => GoToScene(data.TargetSceneId);
+            button.Click += (s, e) => LoadScene(data.TargetSceneId);
+            button.Click += (s, e) => SpecialFunctions(data);
+            button.Opacity = 0;
+            button.MouseEnter += (s, e) => button.Opacity = 0.5;
+            button.MouseLeave += (s, e) => button.Opacity = 0;
             buttonlist.Add(button);
             main.Children.Add(button);
+        }
+
+        public async Task SpecialFunctions(ButtonData data)
+        {
+            if (data.SpecialId == 1)
+            {
+                if (hasKey == true)
+                {
+                    LoadScene(2);
+                }
+                else
+                {
+                    await LaunchDialogue("A sakra, je to zamčené. Potřebuji klíč.", 5);
+                }
+            }
+
+            else if (data.SpecialId == 2)
+            {
+                await LaunchDialogue("angd... hmm, co to může znamenat?", 7);
+            }
+
+            else if (data.SpecialId == 3)
+            {
+                LoadScene(5);
+                await LaunchDialogue("Co to je za nástroj? To neznám...", 4);
+            }
+            else if (data.SpecialId == 4)
+            {
+                if (letterRead == true)
+                {
+                    if (alreadySwitched1 == true)
+                    {
+                        LoadScene(6);
+                    }
+                    else
+                    {
+                        sceny.SwitchChapter(6);
+                        alreadySwitched1 = true;
+                    }
+                }
+                else
+                {
+                    await LaunchDialogue("Proč bych tam chodil? Zatím tam nic nenajdu.", 5);
+                }
+            }
+            else if (data.SpecialId == 5)
+            {
+                LoadScene(2);
+                await LaunchDialogue("Aha! Takže do lesa zmizela... Musím tam!", 5);
+                letterRead = true;
+            }
+            else if (data.SpecialId == 6)
+            {
+                await LaunchDialogue("Ne. Tudy cesta nevede.", 3);
+            }
+
+            else if (data.SpecialId == 7)
+            {
+                LoadScene(7);
+                await LaunchDialogue("Zdá se, že tohle je vše co autor hry stihl udělat. Tak dnes tu záhadu asi nevyřeším.", 10);
+                await Task.Delay(15000);
+                EnterPressed();
+            }
+            else if (data.SpecialId == 8)
+            {
+                barmanTalk = true;
+                dialoguebar.FontSize = 50;
+                LoadScene(1);
+                await LaunchDialogue("Ahoj! Hm, Tebe neznám. Ty jsi tu určitě kvůli Anežce, té zmizelé dívce, že? Někde by tu měla být její matka, zkus ji najít a třeba ti mohla pomoct.", 12);
+                dialoguebar.FontSize = 72;
+            }
+            else if (data.SpecialId == 101)
+            {
+                LoadScene(1);
+                await LaunchDialogue("Co tu vokouníš?! Nevidíš, že zde vedeme debatu. Odpal.", 5);
+            }
+            else if (data.SpecialId == 102)
+            {
+                LoadScene(1);
+                await LaunchDialogue("Ještě jsem na nic nepřišel, ale už mám docela žízeň. Ne, nejprv povinnosti, potom zábava.", 5);
+            }
+            else if (data.SpecialId == 103)
+            {
+                clownTalk = true;
+                LoadScene(1);
+                await LaunchDialogue("Král do boje táh, do veliké dálky...", 5);
+            }
+            else if (data.SpecialId == 104)
+            {
+                LoadScene(1);
+                await LaunchDialogue("Ten muzikant je zvláštní, bojím se ho. Něco mi na něm nehraje...", 5);
+            }
+            else if (data.SpecialId == 105)
+            {
+                LoadScene(1);
+                await LaunchDialogue("Na zdraví!", 3);
+            }
+            else if (data.SpecialId == 106)
+            {
+                if (!barmanTalk || !clownTalk)
+                { 
+                    dialoguebar.FontSize = 50;
+                    LoadScene(1);
+                    await LaunchDialogue("(Dáma nevypadá, že by si chtěla povídat. Tip: Zkus si prvně promluvit s ostatními postavami.)", 10);
+                    dialoguebar.FontSize = 72;
+                }
+                else
+                {
+                    hasKey = true;
+                    dialoguebar.FontSize = 50;
+                    LoadScene(90);
+                    await LaunchDialogue("Pozdrav Pánbůh, vy budete ten detektiv, že? Prosím, pomozte mi najít mou dceru! Zde je klíč od mého domu, třeba v něm něco najdete.", 15);
+                    dialoguebar.FontSize = 72;
+                }
+            }
         }
 
 
@@ -193,6 +292,7 @@ namespace WpfApp1
                 animace.FadeOut();
             });
             File.WriteAllText("save.json", "0");
+            LaunchDialogue("Slyšel jsem, že se z této vesnice ze dne na den vypařila dívka. Musím ji najít!", 10);
         }
 
         public void Launch_Click(object sender, RoutedEventArgs e)
@@ -201,7 +301,7 @@ namespace WpfApp1
             animace.FadeIn(() =>
             {
                 title_menu.Visibility = Visibility.Collapsed;
-                pozadi.Source = new BitmapImage(new Uri($"pack://application:,,,/img/{progress}/0/backdrop.png"));
+                LoadScene(progress);
                 game.Visibility = Visibility.Visible;
                 animace.FadeOut();
             });
@@ -210,6 +310,7 @@ namespace WpfApp1
         public void Continue_Click(object sender, RoutedEventArgs e)
         {
             pause_menu.Visibility = Visibility.Collapsed;
+            LoadScene(currentSceneId);
         }
 
         public void Quit_Click(object sender, RoutedEventArgs e)
@@ -236,9 +337,16 @@ namespace WpfApp1
                     {
                         pause_menu.Visibility = Visibility.Collapsed;
                         game.Visibility = Visibility.Visible;
+                        LoadScene(progress);
                     }
                     else
                     {
+                        progress = currentSceneId;
+                        WriteSave();
+                        foreach (var btn in buttonlist)
+                            main.Children.Remove(btn);
+                        buttonlist.Clear();
+
                         pause_menu.Visibility = Visibility.Visible;
                         game.Visibility = Visibility.Collapsed;
                     }
@@ -246,8 +354,44 @@ namespace WpfApp1
             }
         }
 
-        private const string spravnykod = "1234";
-        private string enteredCode = "";
+        public void EnterPressed()
+        {
+            if (currentSceneId == 7)
+            {
+                progress = 0;
+                WriteSave();
+                Rectangle end = new Rectangle();
+                end.Fill = Brushes.Black;
+                end.HorizontalAlignment = HorizontalAlignment.Stretch;
+                end.VerticalAlignment = VerticalAlignment.Stretch;
+                main.Children.Add(end);
+
+                TextBlock thx = new TextBlock();
+                thx.Foreground = Brushes.White;
+                thx.HorizontalAlignment = HorizontalAlignment.Center;
+                thx.VerticalAlignment = VerticalAlignment.Center;
+                thx.Text = "Děkuji za zahrání! Byť jsi to dokončil za asi 3 minuty...";
+                thx.FontFamily = new FontFamily("Juice ITC");
+                thx.FontWeight = FontWeights.Bold;
+                thx.TextWrapping = TextWrapping.Wrap;
+                thx.FontSize = 72;
+                thx.TextAlignment = TextAlignment.Center;
+                thx.Width = 450;
+                thx.Visibility = Visibility.Visible;
+                main.Children.Add(thx);
+            }
+            else
+            {
+                dialoguebg.Visibility = Visibility.Hidden;
+                dialoguebar.Visibility = Visibility.Hidden;
+                dialoguehelp.Visibility = Visibility.Hidden;
+            }
+        }
+
+
+
+
+
 
         public void Num_Click(object sender, RoutedEventArgs e)
         {
@@ -259,7 +403,8 @@ namespace WpfApp1
         {
             if (enteredCode == spravnykod)
             {
-                GoToScene(4);
+                LoadScene(4);
+                trezorLock = false;
             }
             else
             {
@@ -270,7 +415,7 @@ namespace WpfApp1
         public void WrongCode()
         {
             enteredCode = "";
-            MessageBox.Show("Zadal jsi špatný kód.", "Chyba");
+            MessageBox.Show("Zadal jsi špatný kód. Zkus to znovu. (Nápověda: Kód má 5 čísel)", "Chyba");
         }
     }
 }
--- Scenes.cs
+++ Scenes.cs
@@ -10,13 +10,11 @@ namespace WpfApp1
     public class Scenes
     {
         MainWindow main;
-        Dialogues dialogy;
         Animations animace;
-        public Scenes(MainWindow mainWindow, Dialogues dialogues, Animations animacie)
+        public Scenes(MainWindow mainWindow, Animations animacie)
         {
             main = mainWindow;
             animace = animacie;
-            dialogy = dialogues;
         }
 
         public class Scene
@@ -30,9 +28,7 @@ namespace WpfApp1
         {
             public int X, Y, Width, Height;
             public int TargetSceneId;
-            public bool IsDialogue = false;
-            public int DialogueId;          
-            public int Sleep;
+            public int SpecialId;
         }
 
         public List<Scene> scenes = new()
@@ -43,8 +39,11 @@ namespace WpfApp1
         BackgroundPath = "pack://application:,,,/img/0/0/backdrop.png",
         Buttons = new()
         {
-            new ButtonData { X = 1228, Y = 769, Width = 70, Height = 200, TargetSceneId = 1 },
-            new ButtonData { X = 773, Y = 792, Width = 25, Height = 130, TargetSceneId = 2 },
+            new ButtonData { X = 1228, Y = 809, Width = 70, Height = 200, TargetSceneId = 1 },
+            new ButtonData { X = 773, Y = 852, Width = 25, Height = 130, SpecialId = 1 },
+            new ButtonData { X = 1585, Y = 850, Width = 160, Height = 150, SpecialId = 4 },
+            new ButtonData { X = 368, Y = 809, Width = 222, Height = 200, SpecialId = 6 },
+            new ButtonData { X = 1441, Y = 850, Width = 44, Height = 130, SpecialId = 6 },
         }
     },
     new Scene
@@ -53,7 +52,14 @@ namespace WpfApp1
         BackgroundPath = "pack://application:,,,/img/0/1/backdrop.png",
         Buttons = new()
         {
-            new ButtonData { X = 1248, Y = 183, Width = 57, Height = 133, TargetSceneId = 0 },
+            new ButtonData { X = 1700, Y = 230, Width = 77, Height = 170, TargetSceneId = 0 },
+            new ButtonData { X = 830, Y = 540, Width = 99, Height = 184, SpecialId = 8 },
+            new ButtonData { X = 300, Y = 620, Width = 123, Height = 155, SpecialId = 101 },
+            new ButtonData { X = 600, Y = 600, Width = 111, Height = 111, SpecialId = 102 },
+            new ButtonData { X = 1650, Y = 505, Width = 177, Height = 360, SpecialId = 103 },
+            new ButtonData { X = 1567, Y = 585, Width = 50, Height = 170, SpecialId = 104 },
+            new ButtonData { X = 1414, Y = 730, Width = 160, Height = 400, SpecialId = 105 },
+            new ButtonData { X = 1234, Y = 600, Width = 50, Height = 130, SpecialId = 106}, //matka dcery, dodělat
         }
     },
     new Scene
@@ -62,9 +68,10 @@ namespace WpfApp1
         BackgroundPath = "pack://application:,,,/img/0/2/backdrop.png",
         Buttons = new()
         {
-            new ButtonData {X = 288, Y = 576, Width = 40, Height = 30, IsDialogue = true, DialogueId = 0, TargetSceneId = 2},
-            new ButtonData {X = 1258, Y = 271, Width = 94, Height = 460, TargetSceneId = 0 },
-            new ButtonData {X = 953, Y = 400, Width = 67, Height = 82, TargetSceneId = 3 },
+            new ButtonData {X = 373, Y = 737, Width = 100, Height = 40, TargetSceneId = 2, SpecialId = 2},
+            new ButtonData {X = 50, Y = 465, Width = 100, Height = 130, SpecialId = 3},
+            new ButtonData {X = 1718, Y = 326, Width = 134, Height = 610, TargetSceneId = 0 },
+            new ButtonData {X = 1300, Y = 515, Width = 107, Height = 100, TargetSceneId = 3 },
         }
     },
 
@@ -74,7 +81,7 @@ namespace WpfApp1
         BackgroundPath = "pack://application:,,,/img/0/3/dackbrop.png",
         Buttons = new()
         {
-            new ButtonData {X = 30, Y = 312, Width = 90, Height = 90, TargetSceneId = 2 },
+            new ButtonData {X = 60, Y = 33, Width = 265, Height = 150, TargetSceneId = 2 },
         }
     },
 
@@ -84,17 +91,60 @@ namespace WpfApp1
         BackgroundPath = "pack://application:,,,/img/0/4/backdrop.png",
         Buttons = new()
         {
-            new ButtonData {X = 30, Y = 312, Width = 90, Height = 90, TargetSceneId = 2 },
+            new ButtonData {X = 85, Y = 46, Width = 350, Height = 222, SpecialId = 5 },
+        }
+    },
+
+        new Scene
+    {
+        Id = 5,
+        BackgroundPath = "pack://application:,,,/img/0/5/backdrop.png",
+        Buttons = new()
+        {
+            new ButtonData {X = 85, Y = 46, Width = 350, Height = 222, TargetSceneId = 2 },
+        }
+    },
+
+        new Scene
+    {
+        Id = 6,
+        BackgroundPath = "pack://application:,,,/img/1/0/backdrop.png",
+        Buttons = new()
+        {
+            new ButtonData {X = 760, Y = 1111, Width = 400, Height = 100, TargetSceneId = 0 },
+            new ButtonData {X = 567, Y = 650, Width = 255, Height = 350, SpecialId = 7 },
+        }
+    },
+        new Scene
+    {
+        Id = 7,
+        BackgroundPath = "pack://application:,,,/img/1/1/backdrop.png",
+        Buttons = new()
+        {
+            
+        }
+    },
+
+        new Scene
+    {
+        Id = 90,
+        BackgroundPath = "pack://application:,,,/img/0/90/backdrop.png",
+        Buttons = new()
+        {
+            new ButtonData {X = 85, Y = 46, Width = 350, Height = 222, TargetSceneId = 1 },
         }
     },
 };
 
-        public void SwitchScene()
+        public void SwitchChapter(int scene)
         {
-            animace.FadeIn();
-            main.buttonlist.Clear();
-            main.progress++;
-            main.WriteSave();
+            animace.FadeIn(() =>
+            {
+                main.WriteSave();
+                main.LoadScene(scene);
+                main.progress++;
+                animace.FadeOut();
+            });
         }
     }
 }
